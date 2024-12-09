dacef04eb4ab9fa395e3cd7096dc5bfb5f6ae9f1
Waheed-Mousad <whed555@hotmail.com>
2024-12-09 12:30:04 +0300

    fixed bug and added comments

--- main.py
+++ main.py
@@ -28,13 +28,17 @@ from PlottingMethods.SchedulingPlotting import plot_turn_around_vs_waiting_time
 import time
 from colorama import Fore, Style, init
 if __name__ == '__main__':
-    init()
-    data1_models = {}
-    data2_models = {}
-    data_models = [data1_models, data2_models]
-    predictions_data1 = {}
-    predictions_data2 = {}
-    predictions = [predictions_data1, predictions_data2]
+    init()  # initialize colorama
+    data1_models = {}   # dictionary to store the models for dataset 1, the key is the model name and the value is the model object
+    data2_models = {}   # dictionary to store the models for dataset 2, the key is the model name and the value is the model object
+    data_models = [data1_models, data2_models]  # list of dictionaries to store the models for both datasets, this was created to reduce the code redundancy
+    """
+    if for example we want to access the linear regression model for dataset 1 we can do the following: data_models[0]['Linear Regression from scratch']
+    basically calling the list that has the dictonary for dataset 1 and 2, than inside the dictionary we call the model using it is key
+    """
+    predictions_data1 = {}  # dictionary to store the predictions for dataset 1, the key is the model name and the value is the predicted dataframe
+    predictions_data2 = {}  # dictionary to store the predictions for dataset 2, the key is the model name and the value is the predicted dataframe
+    predictions = [predictions_data1, predictions_data2]    # list of dictionaries to store the predictions for both datasets, this was created to reduce the code redundancy
     GREEN = Fore.GREEN  # Green color
     RESET = Style.RESET_ALL  # Reset to default color
     RED = Fore.RED  # Red color
@@ -55,7 +59,9 @@ if __name__ == '__main__':
         SVR = 6
         Neural Network = 7
         0 for all models
-        :return:
+        :param datasetint: integer that tell the function which dataset to work train and predict on
+        :return: return nothing, just add the model and prediciton to the data_models and predictions dictionaries
+        ths function was made to reduce the code redundancy
         """
 
         if model == 0:
@@ -69,14 +75,16 @@ if __name__ == '__main__':
             train_model(dataset, 7, datasetint)
         if model == 1:
             print(f"{GREEN}######Training Linear Regression from scratch######{RESET}")
-            if 'Linear Regression from scratch' in data_models[datasetint]:
+            if 'Linear Regression from scratch' in data_models[datasetint]: # check if the model has been trained before
                 print(f"{RED}Model already trained{RESET}")
-                print(f"{GREEN}Model test MAE:{RED} {data_models[datasetint]['Linear Regression from scratch'].mae_test}{RESET}")
+                print(f"{GREEN}Model test MAE:{RED} {data_models[datasetint]['Linear Regression from scratch'].mae_test}{RESET}") # print the MAE of the model
 
             else:
+                # create the model object and train it and save it in the data_models dictionary
                 linearRegScratch = LineatRegressionScratch()
                 linearRegScratch.train(*dataset)
                 data_models[datasetint]['Linear Regression from scratch'] = linearRegScratch
+                # predict the data and save it in the predictions dictionary
                 predictions[datasetint]['Linear Regression from scratch'] = linearRegScratch.predict(dataset[2])
 
         if model == 2:
@@ -144,8 +152,9 @@ if __name__ == '__main__':
 
     # Read the data
     df = pd.read_csv(os.path.join('MachineLearningModels','processes_datasets.csv'))
-    #Re index so index = JobID
+    #Re index so index = JobID or basically start from 1 instead of 0
     df.index = df.index + 1
+    # Preprocess the data  and create data 1 and 2
     data1, mapping1 = data_preprocess(df, target_col=[
         'SubmitTime', 'WaitTime', 'AverageCPUTimeUsed', 'Used Memory',
         'ReqTime: ', 'ReqMemory', 'Status', 'UserID', 'RunTime '
@@ -169,7 +178,8 @@ if __name__ == '__main__':
         print(f"{YELLOW}1{RESET}. {MAGENTA}Dataset 1 with 100 samples{RESET}")
         print(f"{YELLOW}2{RESET}. {MAGENTA}Dataset 2 with 1000 samples{RESET}")
         print(f"{YELLOW}3{RESET}. {RED}Exit{RESET}")
-        choice = input()
+        choice = input() # get the user input to choose the dataset
+        # this string will be printed at multiple places in the code so it is better to store it in a variable to reduce redundancy
         models_print = (f"{YELLOW}0{RESET}. {MAGENTA}All{RESET}\n"
                         f"{YELLOW}1{RESET}. {MAGENTA}Linear Regression from scratch{RESET}\n"
                         f"{YELLOW}2{RESET}. {MAGENTA}Linear Regression from library{RESET}\n"
@@ -182,38 +192,44 @@ if __name__ == '__main__':
 
         if choice == '1':
             print(f"{GREEN}######Working on Dataset 1######{RESET}")
-            while True:
+            while True: # loop to train the model based on the user input
                 print(f"{CYAN}Which model would you like to train?{RESET}")
                 print(models_print)
-                model_choice = input()
-                try:
+                model_choice = input() # get the user input to choose the model
+                try: # make sure the user input is valid
                     int(model_choice)
-                except ValueError:
+                except ValueError: # if the user input is not a number
                     print(f"{RED}Invalid choice{RESET}")
                     time.sleep(0.5)
                     continue
-                if int(model_choice) == 8:
+                if int(model_choice) == 8: # if the user choose to exit
                     print(f"{RED}Exiting...{RESET}")
                     time.sleep(0.5)
                     break
-                if int(model_choice) not in range(9):
+                if int(model_choice) not in range(9): # if the user input is not in the legal range
                     print(f"{RED}Invalid choice{RESET}")
                     time.sleep(0.5)
                     continue
+                # train the model based on the user input after passing all the checks
                 train_model(split1, int(model_choice), 0)
                 time.sleep(0.5)
-            # TODO tell the user what model has been trained and ask which one he wish to schedule show schedule for
-            print(f"{RED}scheduling...{RESET}")
+            # make sure the user trained at least one model before scheduling
+            if len(predictions_data1) == 0:
+                print(f"{RED}No model has been trained, will train default model{RESET}")
+                train_model(split1, 1, 0)
+                time.sleep(0.5)
+            # schedule the models, basically adding a new column to that store the waiting time and turn around time for each scheduling algorithm
 
+            print(f"{RED}scheduling...{RESET}")
             for key, value in predictions_data1.items():
-                try:
-                    value = denormlize_data(value, mapping1)
-                    temp = [[],[]]
-                    df, avg_waiting_time, avg_turn_around_time = SJF(value, value.columns[-1])
-                    temp[0] = [df, avg_waiting_time, avg_turn_around_time]
-                    df, avg_waiting_time, avg_turn_around_time = FCFS(value, value.columns[-1])
-                    temp[1] = [df, avg_waiting_time, avg_turn_around_time]
-                    predictions_data1[key] = temp
+                try: # make sure the data is not already scheduled
+                    value = denormlize_data(value, mapping1) # denormlize the data
+                    temp = [[],[]] # create a temporary list to store the data
+                    df, avg_waiting_time, avg_turn_around_time = SJF(value, value.columns[-1]) # schedule the data using SJF
+                    temp[0] = [df, avg_waiting_time, avg_turn_around_time] # store the scheduled data and the average waiting time and turn around time using SJF at the first place in the list
+                    df, avg_waiting_time, avg_turn_around_time = FCFS(value, value.columns[-1]) # schedule the data using FCFS
+                    temp[1] = [df, avg_waiting_time, avg_turn_around_time] # store the scheduled data and the average waiting time and turn around time using FCFS at the second place in the list
+                    predictions_data1[key] = temp # store the list in the predictions_data1 dictionary, this will turn the value which used to be just a dataframe to a list
                 except:
                     print(f"{RED}{key} already scheduled skipping to next...{RESET}")
                     continue
@@ -222,9 +238,9 @@ if __name__ == '__main__':
             while True:
                 print(f"{CYAN}Which model would you like to plot the schedule for?{RESET}")
                 print(f"{YELLOW}0{RESET}. {MAGENTA}All available models{RESET}")
-                value = list(predictions_data1.values())
-                keys = list(predictions_data1.keys())
-                for index, key in enumerate(keys, start=1):
+                value = list(predictions_data1.values()) # get the values of the dictionary and store it in a list
+                keys = list(predictions_data1.keys()) # get the keys of the dictionary and store it in a list
+                for index, key in enumerate(keys, start=1): # loop thro the keys and print them (this make sure to catch what models were trained and have dynamic input)
                     print(f"{YELLOW}{index}{RESET}. {MAGENTA}{key}{RESET}")
                 print(f"{YELLOW}{(len(value)+1)}{RESET}. {RED}Exit{RESET}")
 
@@ -240,11 +256,11 @@ if __name__ == '__main__':
                     print(f"{RED}Invalid choice{RESET}")
                     time.sleep(0.5)
                     continue
-                if int(choice) == index + 1:
+                if int(choice) == index + 1: # if the user choose to exit
                     print(f"{RED}Exiting...{RESET}")
                     time.sleep(0.5)
                     break
-                if int(choice) == 0:
+                if int(choice) == 0: # if the user choose to plot all the models which is not implemented yet TODO
                     print(f"{GREEN}Plotting all models not implemented yet :){RESET}")
                     time.sleep(0.5)
                     continue
@@ -277,12 +293,17 @@ if __name__ == '__main__':
                 train_model(split2, int(model_choice), 1)
                 time.sleep(0.5)
             # TODO tell the user what model has been trained and ask which one he wish to schedule
+            if len(predictions_data2) == 0:
+                print(f"{RED}No model has been trained, will train default model{RESET}")
+                train_model(split2, 1, 1)
+                time.sleep(0.5)
             print(f"{RED}scheduling...{RESET}")
             for key, value in predictions_data2.items():
                 try:
                     value = denormlize_data(value, mapping2)
                     temp = [[],[]]
                     df, avg_waiting_time, avg_turn_around_time = SJF(value, value.columns[-1])
+                    print(value.columns[-1])
                     temp[0] = [df, avg_waiting_time, avg_turn_around_time]
                     df, avg_waiting_time, avg_turn_around_time = FCFS(value, value.columns[-1])
                     temp[1] = [df, avg_waiting_time, avg_turn_around_time]
